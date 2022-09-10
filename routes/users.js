bddfd5b2c288726f172a4b417b41a6739e84cf6d
Aleksey Paramonov <paramonovav90@gmail.com>
2022-09-10 14:52:53 +0400

    added required for 'name'

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ const {
 
 userRouter.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
